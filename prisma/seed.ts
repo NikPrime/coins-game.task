1d74ae70555bcf51ae6e06a9046c3607b93414d1
Nikita Antipin <n.antipin@admon.ai>
2024-01-10 16:25:28 +0300

    Generate prizma client for current platform + made seeds upsert

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,30 +4,46 @@ const prisma = new PrismaClient();
 
 async function main() {
     await Promise.all([
-        prisma.user.create({
-            data: {
+        prisma.user.upsert({
+            where: {
+                email: 'vladimir@example.com',
+            },
+            update: {},
+            create: {
                 id: uuidv4(),
                 email: 'vladimir@example.com',
                 name: 'Vladimir',
             },
         }),
-        prisma.user.create({
-            data: {
+        prisma.user.upsert({
+            where: {
+                email: 'ivan@example.com',
+            },
+            update: {},
+            create: {
                 id: uuidv4(),
                 email: 'ivan@example.com',
                 name: 'Ivan',
             },
         }),
-        await prisma.product.create({
-            data: {
-                id: uuidv4(),
+        await prisma.product.upsert({
+            where: {
+                id: 'd2f7d739-972b-4846-aa85-6dc7cb502891'
+            },
+            update: {},
+            create: {
+                id: 'd2f7d739-972b-4846-aa85-6dc7cb502891',
                 name: 'Bread',
                 price: 100,
             },
         }),
-        await prisma.product.create({
-            data: {
-                id: uuidv4(),
+        await prisma.product.upsert({
+            where: {
+                id: '27427eb4-e637-4136-9f04-1186ca8790d9',
+            },
+            update: {},
+            create: {
+                id: '27427eb4-e637-4136-9f04-1186ca8790d9',
                 name: 'Butter',
                 price: 250,
             },
@@ -36,8 +52,7 @@ async function main() {
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
+    .catch(() => {
         process.exit(1);
     })
     .finally(async () => {
